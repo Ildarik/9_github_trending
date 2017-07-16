a17cd553653a0667b6b74051e672c224b8572959
Nogmanov Ildar <nogmanov@gmail.com>
2017-07-16 22:02:02 +0300

    Add body for get_open_issues_amount()

--- github_trending.py
+++ github_trending.py
@@ -3,14 +3,16 @@ from datetime import datetime, timedelta
 
 
 def get_trending_repositories(top_size):
-    base_url = 'https://api.github.com/search/repositories?'
     datetime_7_days_ago = datetime.now() - timedelta(days=7)
-    r = requests.get(base_url + 'q=created:>' + str(datetime_7_days_ago.date()) + '&sort=stars')
+    r = requests.get('https://api.github.com/search/repositories?' + 'q=created:>' + str(datetime_7_days_ago.date()) + '&sort=stars')
     repos = r.json()['items']
     return repos[:top_size]
 
+
 def get_open_issues_amount(repo_owner, repo_name):
-    pass
+    r = requests.get('https://api.github.com/repos/' + repo_owner + '/' + repo_name)
+    return r.json()['open_issues_count']
+
 
 if __name__ == '__main__':
-    get_trending_repositories(20)
+    pass
