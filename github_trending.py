409bcddca30496301714f8c05a6c4c651daa437c
Nogmanov Ildar <nogmanov@gmail.com>
2017-07-16 22:55:43 +0300

    Rename variables and add base_url var

--- github_trending.py
+++ github_trending.py
@@ -4,14 +4,15 @@ from datetime import datetime, timedelta
 
 def get_trending_repositories(top_size):
     datetime_7_days_ago = datetime.now() - timedelta(days=7)
-    r = requests.get('https://api.github.com/search/repositories?' + 'q=created:>' + str(datetime_7_days_ago.date()) + '&sort=stars')
-    repos = r.json()['items']
+    base_url = 'https://api.github.com/search/repositories?'
+    trending_repositories = requests.get(base_url + 'q=created:>' + str(datetime_7_days_ago.date()) + '&sort=stars')
+    repos = trending_repositories.json()['items']
     return repos[:top_size]
 
 
 def get_open_issues_amount(repo_owner, repo_name):
-    r = requests.get('https://api.github.com/repos/' + repo_owner + '/' + repo_name)
-    return r.json()['open_issues_count']
+    repo_info = requests.get('https://api.github.com/repos/' + repo_owner + '/' + repo_name)
+    return repo_info.json()['open_issues_count']
 
 
 if __name__ == '__main__':
