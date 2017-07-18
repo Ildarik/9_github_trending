943e9bb1b08f93492d85189af95133d64ab4535d
Nogmanov Ildar <nogmanov@gmail.com>
2017-07-18 08:07:59 +0300

    Add var for constant

--- github_trending.py
+++ github_trending.py
@@ -18,7 +18,8 @@ def get_open_issues_amount(repo_owner, repo_name):
 
 
 if __name__ == '__main__':
-    trending_repositories = get_trending_repositories(20)
+    top_size_repos = 20
+    trending_repositories = get_trending_repositories(top_size_repos)
     for repo in trending_repositories:
         open_issues_amount = get_open_issues_amount(repo['owner']['login'], repo['name'])
         print('Repo url: {}, open issues amount: {}'.format(repo['html_url'], open_issues_amount))
