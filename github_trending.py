34950aa2862006d9c046cedec009b8b7c2f6c7b8
Nogmanov Ildar <nogmanov@gmail.com>
2017-07-18 08:13:08 +0300

    Use format string instead of concatenation

--- github_trending.py
+++ github_trending.py
@@ -6,14 +6,14 @@ def get_trending_repositories(top_size):
     period_of_days = 7
     datetime_period_of_days_ago = datetime.now() - timedelta(days=period_of_days)
     base_url = 'https://api.github.com/search/repositories?'
-    payload = {'q': 'created:>' + str(datetime_period_of_days_ago.date()), 'sort': 'stars'}
+    payload = {'q': 'created:>{}'.format(datetime_period_of_days_ago.date()), 'sort': 'stars'}
     trending_repositories = requests.get(base_url, params=payload)
     repos = trending_repositories.json()['items']
     return repos[:top_size]
 
 
 def get_open_issues_amount(repo_owner, repo_name):
-    repo_info = requests.get('https://api.github.com/repos/' + repo_owner + '/' + repo_name)
+    repo_info = requests.get('https://api.github.com/repos/{}/{}'.format(repo_owner,repo_name))
     return repo_info.json()['open_issues_count']
 
 
